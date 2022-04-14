fda96b85f22de0f42391253fb669b096041537eb
eskendarov <envereskendarov@gmail.com>
2022-04-14 06:12:24 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/eskendarov/concurrent/Wget.java
@@ -0,0 +1,23 @@
+package ru.eskendarov.concurrent;
+
+public class Wget {
+
+    private Wget() {
+    }
+
+    public static void main(final String[] args) {
+        final long timeout = 1000L;
+        final int seconds = 100;
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < seconds; i++) {
+                    Thread.sleep(timeout);
+                    System.out.printf("\rLoading : %d%%", i);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
