ac2d1603f3b9b6cf94a5ea4421bbcc2a9c39bb38
eskendarov <envereskendarov@gmail.com>
2022-07-08 04:20:07 +0300

    fix checkstyle file

--- src/main/java/ru/eskendarov/concurrent/ThreadStop.java
+++ src/main/java/ru/eskendarov/concurrent/ThreadStop.java
@@ -2,7 +2,8 @@ package ru.eskendarov.concurrent;
 
 public class ThreadStop {
 
-    public static void main(final String[] args) throws InterruptedException {
+    public static void main(final String[] args)
+            throws InterruptedException {
         final long timeout = 3000L;
         final Thread thread = new Thread(new ConsoleProgress());
         thread.start();
